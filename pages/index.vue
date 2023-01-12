1c2f12a9f9bfff4dcafaee8a963aaebe12fbf567
abdalla-amin <abdalla.amin@vodafone.com>
2023-01-12 15:12:38 +0200

    redeploy

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,18 @@
 <template>
-<main>
- <h1>Task Board</h1>
- <p>Create a list of tasks</p>
+  <main>
+    <h1>Task Board</h1>
+    <p>Create a list of tasks</p>
 
- <div class="create-new">
-   <input 
-      type="text" 
-      v-model="newTask" 
-      placeholder="Add a new task"
-      @keypress.enter="addTask" />
-   <button @click="addTask">Add</button>
- </div>
+    <div class="create-new">
+      <input type="text" v-model="newTask" placeholder="Add a new task" @keypress.enter="addTask" />
+      <button @click="addTask">Add</button>
+    </div>
 
- <div class="tasks">
-  <Task 
-    v-for="(task, i) in $store.state.tasks" 
-    :key="i" 
-    :task="task" />
- </div>
+    <div class="tasks">
+      <Task v-for="(task, i) in $store.state.tasks" :key="i" :task="task" />
+    </div>
 
-</main>
+  </main>
 </template>
 
 <script>
